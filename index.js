0703a4027d6817925346f1a00272d68541efc0ea
Robinson Khwairakpam <robsonkhzepflo@gmail.com>
2022-06-20 13:51:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+let myLeads=[]
+
+const ulEl = document.getElementById("ul-el")
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query( {active:true, currentWindow:true}, function(tabs) {
+                myLeads.push(tabs[0].url)
+                localStorage.setItem("myLeads",JSON.stringify(myLeads))
+                render(myLeads)
+      })
+})
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads=[]
+    render(myLeads) //clearing out the DOM
+})
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+function render(leads){
+      let listItems=""
+      for(let i=0;i<leads.length;i++){
+      // listItems += "<li><A target='_blank' href='"+ myLeads[i] + "'>" + myLeads[i] + "</A></li>"
+      listItems += `<li>
+                       <a target='_blank' href='${leads[i]}'>
+                       ${leads[i]}
+                       </a>
+                    </li>`
+      }
+ulEl.innerHTML = listItems
+}
